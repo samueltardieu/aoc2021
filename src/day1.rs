b6427eec4561f97fab5babec020cc759bbd13544
Samuel Tardieu <sam@rfc1149.net>
2021-12-01 07:12:41 +0100

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::num::ParseIntError;
 
 #[aoc_generator(day1)]
@@ -6,11 +7,16 @@ fn generator(input: &str) -> Result<Vec<u32>, ParseIntError> {
 }
 
 #[aoc(day1, part1)]
-fn part1(input: &[u32]) -> u32 {
-    0
+fn part1(depths: &[u32]) -> usize {
+    depths.iter().tuple_windows().filter(|(a, b)| b > a).count()
 }
 
 #[aoc(day1, part2)]
-fn part2(input: &[u32]) -> u32 {
-    0
+fn part2(depths: &[u32]) -> usize {
+    let depths = depths
+        .iter()
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .collect_vec();
+    part1(&depths)
 }
