afec827488f09e0aaf3e3f54fc00299970ed946b
Samuel Tardieu <sam@rfc1149.net>
2021-12-09 13:48:06 +0100

    Use bfs_reach for day 9

--- src/day9.rs
+++ src/day9.rs
@@ -1,35 +1,32 @@
-use itertools::Itertools;
-use pathfinding::prelude::Matrix;
-use std::collections::BTreeMap;
+use itertools::{iproduct, Itertools};
+use pathfinding::prelude::{bfs_reach, Matrix};
+
+fn lows(m: &Matrix<u8>) -> impl Iterator<Item = (usize, usize)> + '_ {
+    iproduct!(0..m.rows, 0..m.columns).filter(|k| {
+        let v = m[k];
+        m.neighbours(k, false).all(|n| m[&n] > v)
+    })
+}
 
 #[aoc(day9, part1)]
-fn part1(input: &str) -> i32 {
-    let m = Matrix::from_rows(input.lines().map(|c| c.bytes().map(|c| (c - b'0') as i32))).unwrap();
-    (0..m.rows)
-        .cartesian_product(0..m.columns)
-        .filter_map(|(r, c)| {
-            let v = m[&(r, c)];
-            m.neighbours(&(r, c), false)
-                .all(|n| m[&n] > v)
-                .then(|| v + 1)
-        })
-        .sum()
+fn part1(input: &str) -> u32 {
+    let m = Matrix::from_rows(input.lines().map(|c| c.bytes())).unwrap();
+    lows(&m).map(|k| (m[&k] - b'0') as u32 + 1).sum()
 }
 
 #[aoc(day9, part2)]
 fn part2(input: &str) -> usize {
     let m = Matrix::from_rows(input.lines().map(|c| c.bytes())).unwrap();
-    let mut basins = BTreeMap::<(usize, usize), usize>::new();
-    for k in (0..m.rows).cartesian_product(0..m.columns) {
-        let (mut k, mut v) = (k, m[&k]);
-        while let Some(n) = (v != b'9')
-            .then(|| m.neighbours(&k, false).find(|n| m[n] < v))
-            .flatten()
-        {
-            k = n;
-            v = m[&k];
-        }
-        *basins.entry(k).or_insert(0) += 1;
-    }
-    basins.values().sorted().rev().take(3).product()
+    lows(&m)
+        .map(|n| {
+            bfs_reach(n, |n| {
+                m.neighbours(n, false)
+                    .filter(|k| m[k] != b'9' && m[k] > m[n])
+                    .collect_vec()
+            })
+            .count()
+        })
+        .sorted_unstable_by_key(|&n| std::cmp::Reverse(n))
+        .take(3)
+        .product()
 }
