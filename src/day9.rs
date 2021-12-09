3892d3db599516eb569e5a73bf4949af3143049e
Samuel Tardieu <sam@rfc1149.net>
2021-12-09 13:33:47 +0100

    Simplify parsing code for day 9

--- src/day9.rs
+++ src/day9.rs
@@ -2,19 +2,9 @@ use itertools::Itertools;
 use pathfinding::prelude::Matrix;
 use std::collections::BTreeMap;
 
-fn generator(input: &str) -> Matrix<i32> {
-    let lines = input
-        .lines()
-        .map(|c| c.bytes().map(|c| (c - b'0') as i32).collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-    let mut m = Matrix::new_empty(lines[0].len());
-    lines.into_iter().for_each(|l| m.extend(&l).unwrap());
-    m
-}
-
 #[aoc(day9, part1)]
 fn part1(input: &str) -> i32 {
-    let m = generator(input);
+    let m = Matrix::from_rows(input.lines().map(|c| c.bytes().map(|c| (c - b'0') as i32))).unwrap();
     (0..m.rows)
         .cartesian_product(0..m.columns)
         .filter_map(|(r, c)| {
@@ -28,11 +18,11 @@ fn part1(input: &str) -> i32 {
 
 #[aoc(day9, part2)]
 fn part2(input: &str) -> usize {
-    let m = generator(input);
+    let m = Matrix::from_rows(input.lines().map(|c| c.bytes())).unwrap();
     let mut basins = BTreeMap::<(usize, usize), usize>::new();
     for k in (0..m.rows).cartesian_product(0..m.columns) {
         let (mut k, mut v) = (k, m[&k]);
-        while let Some(n) = (v != 9)
+        while let Some(n) = (v != b'9')
             .then(|| m.neighbours(&k, false).find(|n| m[n] < v))
             .flatten()
         {
