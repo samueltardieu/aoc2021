f48932cb7d57a929ea5ead830f7b095b33ed2924
Samuel Tardieu <sam@rfc1149.net>
2021-12-21 10:41:54 +0100

    Simplify day 21

--- src/day21.rs
+++ src/day21.rs
@@ -10,28 +10,15 @@ fn starting_positions(input: &str) -> (u64, u64) {
 
 #[aoc(day21, part1)]
 fn part1(input: &str) -> u64 {
-    let (mut p1, mut p2) = starting_positions(input);
-    let (mut s1, mut s2, mut d) = (0, 0, 0);
-    for i in 0.. {
-        up(&mut p1, &mut s1, &mut d);
-        if s1 >= 1000 {
-            return (6 * i + 3) * s2;
-        }
-        up(&mut p2, &mut s2, &mut d);
-        if s2 >= 1000 {
-            return (6 * i + 6) * s1;
-        }
-    }
-    0
+    let (p1, p2) = starting_positions(input);
+    score(p1, 0, p2, 0, 0, 0)
 }
 
-fn up(p: &mut u64, s: &mut u64, d: &mut u64) {
-    for _ in 0..3 {
-        *d = *d % 100 + 1;
-        *p += *d;
+fn score(p1: u64, s1: u64, p2: u64, s2: u64, d: u64, c: u64) -> u64 {
+    match (p1 + d * 3 + 5) % 10 + 1 {
+        p1 if s1 + p1 >= 1000 => (c + 3) * s2,
+        p1 => score(p2, s2, p1, s1 + p1, (d + 2) % 100 + 1, c + 3),
     }
-    *p = (*p - 1) % 10 + 1;
-    *s += *p;
 }
 
 #[aoc(day21, part2)]
@@ -43,15 +30,15 @@ fn part2(input: &str) -> u64 {
 
 #[cached]
 fn wins(p1: u64, s1: u64, p2: u64, s2: u64) -> (u64, u64) {
-    if s2 < 21 {
-        [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
-            .into_iter()
-            .fold((0, 0), |(v1, v2), (d, n)| {
-                let p1 = (p1 + d - 1) % 10 + 1;
+    [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
+        .into_iter()
+        .fold((0, 0), |(v1, v2), (d, n)| {
+            let p1 = (p1 + d - 1) % 10 + 1;
+            if s1 + p1 >= 21 {
+                (v1 + n, v2)
+            } else {
                 let (w1, w2) = wins(p2, s2, p1, s1 + p1);
                 (v1 + w2 * n, v2 + w1 * n)
-            })
-    } else {
-        (0, 1)
-    }
+            }
+        })
 }
